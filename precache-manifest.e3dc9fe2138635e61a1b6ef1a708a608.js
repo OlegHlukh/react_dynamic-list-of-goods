e13ff03a2ab2a25b9161db32a184362b8cb3f3e6
Oleg Hluhk <glug.oleg@gmail.com>
2021-04-19 09:52:58 +0300

    Updates

---
+++ precache-manifest.e3dc9fe2138635e61a1b6ef1a708a608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6dfa27eaf6ac874220b4791a534abdc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4edb90ce1f84ac66db57",
+    "url": "./static/css/2.d1ab60d4.chunk.css"
+  },
+  {
+    "revision": "21cc84672b6c97ce97a5",
+    "url": "./static/css/main.f6ace2b8.chunk.css"
+  },
+  {
+    "revision": "4edb90ce1f84ac66db57",
+    "url": "./static/js/2.dcbbaab1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dcbbaab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21cc84672b6c97ce97a5",
+    "url": "./static/js/main.515b0285.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
